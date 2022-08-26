e11c741eaa2aea64903bc4d695acf50d4d9d21a5
Jacques Antoine de Cugnac <ja.decugnac@outlook.com>
2022-08-26 12:14:06 +0200

    css design on 'projects' page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root to: "pages#about"
 
   get "/projects", to: "pages#projects"
+  get "/projects/furniture", to: "pages#furniture"
+  get "/projects/castrvm", to: "pages#castrvm"
+  get "/projects/claps", to: "pages#claps"
   get "/skills", to: "pages#skills"
   get "/contact", to: "pages#contact"
 end
